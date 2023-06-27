1465ab57afd2b910ecacec99a685cee5650b3efd
alexvcs <alexcurtisslep@gmail.com>
2023-06-27 12:33:04 -0400

    added endpoints

---
+++ api/userData/userData.router.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+// [CREATE] new userData
+router.post("/", (req, res) => {
+  res.json({ message: "Creating new user data" });
+});
+// [READ] user data
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ message: `Getting user data with id: ${id}` });
+});
+// [UPDATE] the data in user data
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ message: `Updated user data with id: ${id}` });
+});
+// [DELETE] a users data
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ message: `Deleting user data with id: ${id}` });
+});
+// * [READ] ALL of the user data
+router.get("/", (req, res) => {
+  res.json({ message: "Getting all user data" });
+});
+
+module.exports = router;
