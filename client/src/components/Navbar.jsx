7521a4099c202b5afb7ee1c500993f13469a77c5
steve smodish <smodish@hotmail.com>
2023-07-09 16:45:15 -0400

    Less intrusive navbar buttons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,11 +4,17 @@ const Navbar = () => {
   return (
     <div className="text-xl text-center p-4">
       <div className="p-2">🥭 Mango Grammerjam</div>
-      <div className="flex justify-between">
-        <Link to={"/"} className="border rounded-lg p-2">
+      <div className="flex justify-around">
+        <Link
+          to={"/"}
+          className="border h-6 rounded-lg p-1 text-xs flex flex-col justify-center"
+        >
           Form View
         </Link>
-        <Link to={"/dataview"} className="border rounded-lg p-2">
+        <Link
+          to={"/dataview"}
+          className="border h-6 rounded-lg p-1 text-xs flex flex-col justify-center"
+        >
           Data View
         </Link>
       </div>
