d62fd97772c7d40400930d2d4a49f1b529978f08
alexvcs <alexcurtisslep@gmail.com>
2023-07-11 13:43:52 -0400

    sorting in grid view

--- client/src/components/DataView.jsx
+++ client/src/components/DataView.jsx
@@ -1,7 +1,13 @@
 import React, {useEffect, useState} from "react";
+import UserData from "./UserData";
 
 const DataView = () => {
   const [userData, setUserData] = useState([]);
+  const [firstSorted, setFirstSorted] = useState(false);
+  const [lastSorted, setLastSorted] = useState(false);
+  const [numberSorted, setNumberSorted] = useState(false);
+  const [providerSorted, setProviderSorted] = useState(false);
+  const [subDateSorted, setSubDateSorted] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
@@ -11,33 +17,128 @@ const DataView = () => {
     };
     getData();
   }, []);
+
+  useEffect(() => {
+    if (firstSorted) {
+      sortFirstName();
+      setFirstSorted(false);
+    }
+    if (lastSorted) {
+      sortLastName();
+      setLastSorted(false);
+    }
+    if (numberSorted) {
+      sortCardNumber();
+      setNumberSorted(false);
+    }
+    if (providerSorted) {
+      sortCardProvider();
+      setProviderSorted(false);
+    }
+    if (subDateSorted) {
+      sortSubmittedDate();
+      setSubDateSorted(false);
+    }
+  }, [firstSorted, lastSorted, numberSorted, providerSorted, subDateSorted]);
+
+  const sortFirstName = () => {
+    setUserData(
+      userData.sort((a, b) => {
+        let keyA = a.firstName.toLowerCase();
+        let keyB = b.firstName.toLowerCase();
+        if (keyA < keyB) {
+          return -1;
+        }
+        if (keyA > keyB) {
+          return 1;
+        }
+      })
+    );
+  };
+
+  const sortLastName = () => {
+    setUserData(
+      userData.sort((a, b) => {
+        let keyA = a.lastName.toLowerCase();
+        let keyB = b.lastName.toLowerCase();
+        if (keyA < keyB) {
+          return -1;
+        }
+        if (keyA > keyB) {
+          return 1;
+        }
+      })
+    );
+  };
+
+  const sortCardNumber = () => {
+    setUserData(
+      userData.sort((a, b) => {
+        let keyA = a.cardNumber;
+        let keyB = b.cardNumber;
+        if (keyA < keyB) {
+          return -1;
+        }
+        if (keyA > keyB) {
+          return 1;
+        }
+      })
+    );
+  };
+
+  const sortCardProvider = () => {
+    setUserData(
+      userData.sort((a, b) => {
+        let keyA = a.cardProvider.toLowerCase();
+        let keyB = b.cardProvider.toLowerCase();
+        if (keyA < keyB) {
+          return -1;
+        }
+        if (keyA > keyB) {
+          return 1;
+        }
+      })
+    );
+  };
+
+  const sortSubmittedDate = () => {
+    setUserData(
+      userData.sort((a, b) => {
+        let keyA = a.createdAt;
+        let keyB = b.createdAt;
+        if (keyA < keyB) {
+          return -1;
+        }
+        if (keyA > keyB) {
+          return 1;
+        }
+      })
+    );
+  };
+
   return (
     <div className="grid grid-cols-7 justify-items-center	">
-      <div>First Name</div>
-      <div>Last Name</div>
-      <div>Credit Card Number</div>
+      <div className="cursor-pointer" onClick={() => setFirstSorted(true)}>
+        First Name
+      </div>
+      <div className="cursor-pointer" onClick={() => setLastSorted(true)}>
+        Last Name
+      </div>
+      <div className="cursor-pointer" onClick={() => setNumberSorted(true)}>
+        Credit Card Number
+      </div>
       <div>Expiration</div>
       <div>CVC</div>
-      <div>Credit Card Provider</div>
-      <div>Date Submitted</div>
+      <div className="cursor-pointer" onClick={() => setProviderSorted(true)}>
+        Credit Card Provider
+      </div>
+      <div className="cursor-pointer" onClick={() => setSubDateSorted(true)}>
+        Date Submitted
+      </div>
 
       {userData
         ? userData.map((user) => {
-            console.log("user", user);
-            let date = new Date(user.createdAt);
-            return (
-              <React.Fragment key={user._id}>
-                <div>{user.firstName}</div>
-                <div>{user.lastName}</div>
-                <div>{user.cardNumber}</div>
-                <div>
-                  {user.expirationMonth}/{user.expirationYear}
-                </div>
-                <div>{user.cvcNumber}</div>
-                <div>{user.cardProvider}</div>
-                <div>{date.toLocaleDateString()}</div>
-              </React.Fragment>
-            );
+            return <UserData key={user._id} data={{...user}} />;
           })
         : null}
     </div>
---
+++ client/src/components/UserData.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const UserData = ({data}) => {
+  console.log("user", data);
+  let date = new Date(data.createdAt);
+  return (
+    <React.Fragment>
+      <div>{data.firstName}</div>
+      <div>{data.lastName}</div>
+      <div>{data.cardNumber}</div>
+      <div>
+        {data.expirationMonth}/{data.expirationYear}
+      </div>
+      <div>{data.cvcNumber}</div>
+      <div>{data.cardProvider}</div>
+      <div>{date.toLocaleDateString()}</div>
+    </React.Fragment>
+  );
+};
+
+export default UserData;
