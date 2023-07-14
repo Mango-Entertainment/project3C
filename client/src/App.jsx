a2a96ca57dff3c2b9b6cf969d0cea12e21e6f2f9
alexvcs <alexcurtisslep@gmail.com>
2023-07-14 13:37:12 -0400

    adjust the grid of the data view, make it better

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,12 @@ import DataView from "./components/DataView";
 import MainPage from "./components/MainPage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <div className="w-[375px] lg:w-[1440px] mx-auto">
+      <div className="w-full md:mx-auto">
         <Navbar />{" "}
         <Routes>
           <Route element={<DataView />} path="/dataview" />
