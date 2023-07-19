69bf01192e8c007b491958e49decd9ac3cd112c0
steve smodish <smodish@hotmail.com>
2023-07-19 14:57:50 -0400

    Adjust styles for main container and buttons

--- client/src/components/TableView.jsx
+++ client/src/components/TableView.jsx
@@ -27,101 +27,98 @@ const TableView = ({ data, columns }) => {
   });
 
   return (
-    <div className="flex justify-center px-2">
-      <div className="container">
-        <div>
-          <div>
-            Filter:
-            <input
-              type="text"
-              value={filter}
-              onChange={(e) => setFilter(e.target.value)}
-              className="border ml-2"
-            />
-          </div>
-          <table className="border-collapse border-spacing-0 w-full table border border-solid border-lightGrayViolet mt-2">
-            <thead>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <tr key={headerGroup.id} className="border border-b-gradient-a">
-                  {headerGroup.headers.map((header) => (
-                    <th
-                      key={header.id}
-                      onClick={header.column.getToggleSortingHandler()}
-                      className="border p-2 table-cell text-left align-top"
-                    >
-                      {header.isPlaceholder ? null : (
-                        <div className="flex justify-between pr-2">
-                          <div>
-                            {flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                          </div>
-                          <div>
-                            {
-                              { asc: "↑", desc: "↓", false: "↕" }[
-                                header.column.getIsSorted() ?? null
-                              ]
-                            }
-                          </div>
-                        </div>
-                      )}
-                    </th>
-                  ))}
-                </tr>
+    // <div className="flex justify-center px-2">
+    //   <div className="container">
+    <div className="w-full">
+      <div>
+        Filter:
+        <input
+          type="text"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          className="border ml-2"
+        />
+      </div>
+      <table className="border-collapse border-spacing-0 w-full table border border-solid border-lightGrayViolet mt-2">
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id} className="border border-b-gradient-a">
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  onClick={header.column.getToggleSortingHandler()}
+                  className="border p-2 table-cell text-left align-top"
+                >
+                  {header.isPlaceholder ? null : (
+                    <div className="flex justify-between pr-2">
+                      <div>
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      </div>
+                      <div>
+                        {
+                          { asc: "↑", desc: "↓", false: "↕" }[
+                            header.column.getIsSorted() ?? null
+                          ]
+                        }
+                      </div>
+                    </div>
+                  )}
+                </th>
               ))}
-            </thead>
-            <tbody>
-              {table.getRowModel().rows.map((row) => (
-                <tr key={row.id} className="odd:bg-[#eee] even:bg-[#fff]">
-                  {row.getVisibleCells().map((cell) => (
-                    <td
-                      key={cell.id}
-                      className=" p-2 table-cell text-left align-top"
-                    >
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </td>
-                  ))}
-                </tr>
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id} className="odd:bg-[#eee] even:bg-[#fff]">
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  key={cell.id}
+                  className=" p-2 table-cell text-left align-top"
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
               ))}
-            </tbody>
-          </table>
-          <div className="mt-2">
-            <button
-              className="bg-[#ccc] hover:bg-[#eee] font-bold py-2 px-4 rounded border"
-              onClick={() => table.setPageIndex(0)}
-            >
-              First Page
-            </button>
-            <button
-              className="bg-[#ccc] hover:bg-[#eee] font-bold py-2 px-4 rounded border ml-1  disabled:bg-[#ddd] disabled:text-[#999]"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              Previous Page
-            </button>
-            <button
-              className="bg-[#ccc] hover:bg-[#eee] font-bold py-2 px-4 rounded border ml-1  disabled:bg-[#ddd] disabled:text-[#999]"
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-            >
-              Next Page
-            </button>
-            <button
-              className={
-                "bg-[#ccc] hover:bg-[#eee] font-bold py-2 px-4 rounded border ml-1"
-              }
-              onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-            >
-              Last Page
-            </button>
-          </div>
-        </div>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="w-full mt-2">
+        <button
+          className="bg-[#ccc] hover:bg-[#eee] font-bold py-2 px-2 lg:px-4 rounded border text-xs lg:text-base"
+          onClick={() => table.setPageIndex(0)}
+        >
+          First Page
+        </button>
+        <button
+          className="bg-[#ccc] hover:bg-[#eee] font-bold py-2 px-2 lg:px-4 rounded border text-xs lg:text-base ml-1 disabled:bg-[#ddd] disabled:text-[#999]"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous Page
+        </button>
+        <button
+          className="bg-[#ccc] hover:bg-[#eee] font-bold py-2 px-2 lg:px-4 rounded border text-xs lg:text-base ml-1 disabled:bg-[#ddd] disabled:text-[#999]"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next Page
+        </button>
+        <button
+          className={
+            "bg-[#ccc] hover:bg-[#eee] font-bold py-2 px-2 lg:px-4 rounded border text-xs lg:text-base ml-1"
+          }
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+        >
+          Last Page
+        </button>
       </div>
     </div>
+    //   </div>
+    // </div>
   );
 };
 
