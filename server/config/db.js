f8dbec78d2bd0c6a2d55b742ab980502cae64c11
ssmodish <stevesmodish@gmail.com>
2024-01-17 18:24:16 -0500

    Slight change to mongoose connection

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_CONNECT);
+    const conn = await mongoose.connect(process.env.DB_CONNECT, {dbName: "mangoJS"});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
